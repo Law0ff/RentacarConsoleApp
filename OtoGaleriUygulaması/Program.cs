e9dbd2d842ac810379593e0f886fbc6fe7cc4624
Şafak Yiğit <110359680+Law0ff@users.noreply.github.com>
2022-11-07 02:53:51 +0300

    finished

---
+++ OtoGaleriUygulaması/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace OtoGaleriUygulaması
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Uygulama sınıfından çalıştırma metodunu çağırarak uygulamamızı çalışıyoruz.
+            new Uygulama().Calistir();
+        }
+    }
+}
